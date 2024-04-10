e47f1c17b1d9b11c34b355579c6b9065c7c89e70
Faisal Ahmmad <faisal.ahmmad@mdgroup.com>
2024-04-10 21:56:10 +0100

    documents added

--- git/cushon/resources/views/layouts/app.blade.php
+++ git/cushon/resources/views/layouts/app.blade.php
@@ -14,7 +14,6 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script-->
 </head>
 <body>
     <div id="app">
